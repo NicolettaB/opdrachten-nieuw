e50ee8d5f34dd603dd3168d944c72c1195cfc093
NicolettaB <112668@emmauscollege.nl>
2022-06-18 12:30:34 +0000

    proberen

--- opgave-6.js
+++ opgave-6.js
@@ -12,8 +12,8 @@ function mooiRijtje(eindgetal) {
   var uitkomst = ""
   
   // haal hieronder ______ weg en vervang dit door het juiste stukje code
-  for (var teller = 0; ______ ; teller++) {
-
+  for (var teller = 0; teller > eindgetal ; teller++) {
+       uitkomst = teller + 20;
     // typ hier de code die herhaald moet worden
       
   }
